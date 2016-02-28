192302777dd4b8f8e9a4e308941feb8566ca11c1
HJM <452114240@qq.com>
2016-02-28 16:29:34 +0800

    增加进入首页动画，修改欢迎页部分布局，使元素易于移动

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".welcome-circle").click(function(){
+		$("#bg-1").css("opacity",0);
+		$(".logo").addClass(" logo-left-small");
+		$(".logo-font").addClass(" logo-font-change");
+		$(".welcome-circle").addClass(" welcome-circle-change");
+	});
+});
