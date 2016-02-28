a227b3f617b82296dc34b9131adcbafa5ea3b4f4
HJM <452114240@qq.com>
2016-02-28 22:29:13 +0800

    进入首页后，消除欢迎页按钮hover的样式

--- js/main.js
+++ js/main.js
@@ -4,5 +4,11 @@ $(function(){
 		$(".logo").addClass(" logo-left-small");
 		$(".logo-font").addClass(" logo-font-change");
 		$(".welcome-circle").addClass(" welcome-circle-change");
+		$(".welcome-circle").hover(//消除hover的样式
+			function(){},
+			function(){
+				$(".welcome-circle").find("img").addClass("welcome-circle-img-change");
+				$(".small-circle").addClass("small-circle-change");
+			});
 	});
 });
