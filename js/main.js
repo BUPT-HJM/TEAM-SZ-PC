793ef7d84855c1b36b543a183d625e319f54bfc4
HJM <452114240@qq.com>
2016-03-10 21:07:01 +0800

    加快过渡速度，增加菜单栏的收起功能

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 $(function(){
-	$(".welcome-circle").click(function(){
+	$(".welcome-circle").click(function(e){
 		$("#bg-1").css("opacity",0);
-		$(".logo").addClass(" logo-left-small");
-		$(".logo-font").addClass(" logo-font-change");
-		$(".welcome-circle").addClass(" welcome-circle-change");
-		$(".share-join").addClass(" share-join-show");
+		$(".logo").addClass("logo-left-small");
+		$(".logo-font").addClass("logo-font-change");
+		$(".welcome-circle").addClass("welcome-circle-change");
+		$(".share-join").addClass("share-join-show");
 		$(".welcome-circle").hover(//消除hover的样式
 			function(){},
 			function(){
 				$(".welcome-circle").find("img").addClass("welcome-circle-img-change");
 				$(".small-circle").addClass("small-circle-change");
 			});
-		setTimeout(nav_change,1500);
-		setTimeout(introduction_show,2000);
+		setTimeout(nav_change,1000);
+		setTimeout(introduction_show,1500);
+		$(".welcome-circle").unbind("click").bind("click",function(e){
+				if($(".nav-line").hasClass("nav-line-change")){
+					$(".nav-line").removeClass("nav-line-change");
+				}
+				else{
+					$(".nav-line").addClass("nav-line-change");
+				}
+			});
+		return false;
 	});
 	function nav_change(){
-		$(".nav-line").addClass(" nav-line-change");
+		$(".nav-line").addClass("nav-line-change");
 	};
 	function introduction_show(){
-		$(".introduction").addClass(" introduction-show");
+		$(".introduction").addClass("introduction-show");
 	};
 });
