c766df4fb9e19e20b0faa69898c5b5fd28d7052c
HJM <452114240@qq.com>
2016-03-19 15:25:22 +0800

    修复logo移动bug，未解决canvas带来缩小屏幕产生滚动条问题

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(function(){
+    /*
+    欢迎页按钮点击效果
+     */
     $(".welcome-circle").click(function(e){
         $("#bg-1").css("opacity",0);
         $(".logo").addClass("logo-left-small");
         $(".logo-font").addClass("logo-font-change");
         $(".welcome-circle").addClass("welcome-circle-change");
         $(".share-join").addClass("share-join-show");
-        $(".snow").addClass(" snow-hide");
+        $(".snow").addClass(" snow-hide");//雪花消失
         $(".welcome-circle").hover(//消除hover的样式
             function(){},
             function(){
@@ -30,6 +33,10 @@ $(function(){
     function introduction_show(){
         $(".introduction").addClass("introduction-show");
     };
+
+    /*
+    菜单导航点击效果
+     */
     $(".nav-item").click(function(){
         $(".logo-font").addClass(" logo-font-navitem");
         $(".logo").addClass(" logo-contact");
